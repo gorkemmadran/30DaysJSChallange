58c807c3f78e29953e2d833525a07f426193e848
Gorkem Madran <gorkemmadran@hotmail.com>
2023-10-11 15:07:08 +0300

    Day 1 challange tamamlandı

---
+++ introduction.js
@@ -0,0 +1,35 @@
+/* 
+console.log(`Bu yazı External Script ile yazılmıştır`)
+*/
+console.log(typeof `prototurk`)
+console.log(typeof 6)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+let youtubeChannel = `Prototurk`;
+let channelAge = 11
+let isChannelStillActive = true;
+let secondChannel = null
+let channelMoney = undefined
+
+let name = `Gorkem`,
+    job = `Front end Developer`,
+    surname = `Madran`,
+    age = 25,
+    country = `Turkey`
+
+    console.log(name, surname,  job,  age, country)
+
+
+
+
+
+const gravity = 9.81
+const PI = 3.14
+
+
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney)
---
+++ odev.js
@@ -0,0 +1,62 @@
+// Comment can make code readable // Ödev 1 
+
+
+// Welcome to 30DaysOfJavascript   // Ödev 2
+
+
+/*
+Comments make code readeble,
+easy to reuse and informative      // Ödev 3 
+*/
+
+/*
+
+
+let home = "Gorkem Home";  
+let homeRoom = undefined;
+let homeMembers = 4;                  // ÖDEV 4
+let homeMoney = undefined;
+let homeStillActive = true;
+
+
+console.log(typeof home, typeof homeRoom, typeof homeMembers, typeof homeMoney,typeof homeStillActive)  //ÖDEV 5
+
+
+let table;
+let chair;
+let lighter;        //ÖDEV 6
+let leptop;
+
+
+let glass = 2;
+let tea = 10;
+let lamb = 1;               //ÖDEV 7
+let paper = 99;
+
+
+
+let namee = `Gorkem`;
+let surName = `Madran`;
+let statu = undefined;              //ÖDEV 8
+let country = `Turkey`;
+let age = 25;
+ 
+
+
+let names = `Gorkem`,
+    surNames = `Madran`,
+    job = `Front End Developer`,        // ÖDEV 9
+    ages = 25,
+    countrys = `Turkey`;
+
+let myAge = 25;
+let yourAge = 30;
+console.log("I am", age, "years old.", "You are", yourAge, "years old.")
+
+
+*/
+
+  
+
+
+
