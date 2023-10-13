08e6d0e1c41871f1d71cdf5f86de6de7a5a227d0
Gorkem Madran <gorkemmadran@hotmail.com>
2023-10-13 13:48:46 +0300

    Çalışma bitti

---
+++ intorudction.js
@@ -0,0 +1,70 @@
+let num = 3;
+if (num >= 0){
+console.log('Evet 3, 0 dan büyüktür')
+}
+
+let isRaining = true
+if (isRaining){
+console.log('Evet yağmur yağıyor. Şemsiyeni almayı unutma :)')
+}
+
+let isRainingg = false
+if (isRainingg){
+console.log('Evet yağmur yağıyor. Şemsiyeni almayı unutma :)')     // Koşul False ise if bloğu çalışmaz
+}
+
+let number = 5
+if(number >= 5){
+console.log('İf bloğu çalıştı' )                           
+}
+else{
+    console.log('Else bloğu çalıştı')
+}
+
+
+let numb = 20
+if(numb <= 5){
+console.log('İf bloğu çalıştı' )
+}
+else{
+    console.log('Else bloğu çalıştı')
+}
+
+let result = prompt('2 + 5 kaçtır ?', '2 + 5')
+if(result === '7' ){
+alert('Doğru Cevap Tebrikler')
+} else{
+    alert('Ne yazık ki yanlış cevap.')
+}
+
+let nu = 16
+if(nu === 10){
+console.log('1. koşul çalıştı')
+}else if(nu < 10){
+    console.log('2. koşul çalıştı')
+
+} else if(nu === '16'){
+    console.log('3. koşul çalıştı')
+
+}else if(nu != 16){
+    console.log('4. koşul çalıştı')
+
+}else{
+    console.log('Else  çalıştı')
+
+}
+
+let sayi = prompt('4 + 2 Kaç eder')
+switch(sayi){
+    case '7':
+        console.log('Hatalı cevap -Switch')
+        break
+    case '5':
+        console.log('Cok yaklaştın ama yanlış cevap -Switch')
+        break
+    case '6' :
+        console.log('Tebrikler Doğru Cevap')
+        break
+    default :
+        console.log('Bilemedin..')
+}
